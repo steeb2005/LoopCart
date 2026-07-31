9a378215654eb5923c58c9786138a919c03b10f5
anenonjohnsteven3a@gmail.com <anenonjohnsteven3a@gmail.com>
2026-07-31 09:54:19 +0800

    updated cors

--- server/main.py
+++ server/main.py
@@ -34,7 +34,12 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.1.15:5173"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173", 
+        "http://192.168.1.15:5173",
+        "https://loopcart-shop.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True
