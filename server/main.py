0b2725e29dd3bec0f334315904f458de7fd40b11
anenonjohnsteven3a@gmail.com <anenonjohnsteven3a@gmail.com>
2026-07-31 11:34:38 +0800

    forgot to set true and none to set cookies

--- server/main.py
+++ server/main.py
@@ -291,8 +291,8 @@ async def google_auth(payload: GoogleAuthRequest, response: Response):
         key="access_token",
         value=token,
         httponly=True,
-        secure=False, # Change to TRUE in production only works in http currently, switch to true to work for https
-        samesite="lax", # Set to lax if in development none if in production
+        secure=True, # Change to TRUE in production only works in http currently, switch to true to work for https
+        samesite="none", # Set to lax if in development none if in production
         max_age=60*60*24*30 # Set cookie to expire in 30 days
     )
 
@@ -371,8 +371,8 @@ async def login(login_data: LoginRequest, response: Response):
             key="access_token",
             value=token,
             httponly=True,
-            secure=False, # Change to TRUE in production only works in http currently, switch to true to work for https
-            samesite="lax",
+            secure=True, # Change to TRUE in production only works in http currently, switch to true to work for https
+            samesite="none",
             max_age=60*60*24*30
         )
     else:
@@ -380,8 +380,8 @@ async def login(login_data: LoginRequest, response: Response):
             key="access_token",
             value=token,
             httponly=True,
-            secure=False, # Change to TRUE in production only works in http currently, switch to true to work for https
-            samesite="lax",
+            secure=True, # Change to TRUE in production only works in http currently, switch to true to work for https
+            samesite="none",
             max_age=60*60*24
         )
 
@@ -409,8 +409,8 @@ async def login(login_data: LoginRequest, response: Response):
 async def logout(response: Response):
     response.delete_cookie(
         "access_token",
-        secure=False,
-        samesite="lax"
+        secure=True,
+        samesite="none"
         )
     return{"success": True}
 
